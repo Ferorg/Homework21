c469e21f4c71722cc2a95d68e0600fb5330ddfbe
Ferorg <123727750+Ferorg@users.noreply.github.com>
2023-05-21 18:56:18 +0300

    hh

--- src/Amerikano.java
+++ src/Amerikano.java
@@ -1,18 +1,3 @@
 public class Amerikano extends Coffee {
-    public Amerikano(int kolMilk, int kolWater, TypeCoffee typeCoffee) {
-        super(kolMilk, kolWater, typeCoffee);
-    }
 
-    @Override
-    public void cretedCoffee() {
-        System.out.println("Вы заказали кофе " + typeCoffee);
-    }
-
-    public String toString() {
-        return "\nAmerikano{" +
-                "kolMilk" + getKolMilk() +
-                "kolWater" + getKolWater() +
-                "typeCoffee=" + typeCoffee +
-                '}';
-    }
 }
--- src/CoffeeFabrick.java
+++ src/CoffeeFabrick.java
@@ -1,29 +1,2 @@
 public class CoffeeFabrick implements CoffeFabrick {
-    @Override
-    public Coffee createdCoffee(TypeCoffee typeCoffee) {
-        Coffee coffee = null;
-        CoffeeHouse coffeeHouse = CoffeeHouse.getInstance();
-        switch (typeCoffee) {
-            case AMERICANO:
-                coffee = new Amerikano(0, 90, typeCoffee);
-                coffee.cretedCoffee();
-                break;
-            case LATTE:
-                coffee = new Latte(45, 35, typeCoffee);
-                coffee.cretedCoffee();
-                break;
-            case ESPRESSO:
-                coffee = new Epresso(0, 35, typeCoffee);
-                coffee.cretedCoffee();
-                break;
-            case CAPUCHINO:
-                coffee = new Capuchino(17, 35, typeCoffee);
-                coffee.cretedCoffee();
-                break;
-        }
-        coffeeHouse.addCoffeeInMenuZakaza(coffee);
-        return coffee;
-    }
-
-
 }
